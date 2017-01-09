42f5400c9af3868864aa0777517a8e9e0c955695
seiya tanaka <tanak-se@spp.co.jp>
2017-01-09 16:58:59 +0900

    modivy vim setup

--- vim_setup.sh
+++ vim_setup.sh
@@ -3,3 +3,5 @@ ln -sf ~/github/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/github/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/github/dotfiles/.vim ~/.vim
 
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
