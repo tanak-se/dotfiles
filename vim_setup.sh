e796e80b63cfcf19e3dbbfc9433ebd981a90b723
seiya tanaka <tanak-se@spp.co.jp>
2017-01-11 19:23:23 +0900

    modify setup

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+
 ln -sf ~/github/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/github/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/github/dotfiles/.vim ~/.vim
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-sh ./install.sh
